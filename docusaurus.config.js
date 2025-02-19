078e101c31607633a629715abab5678d0b757c0d
ardhiee <ardhiee@gmail.com>
2025-02-19 11:16:21 +0800

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/ardhiee/java-docu',
+  url: 'https://ardhiee.github.io/java-docu/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',

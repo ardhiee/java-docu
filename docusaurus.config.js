e30e7c46d4028300b3201a5fc6678b12f34cbd16
ardhiee <ardhiee@gmail.com>
2025-02-19 11:20:04 +0800

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ardhiee', // Usually your GitHub org/user name.
-  projectName: 'my java docs', // Usually your repo name.
+  projectName: 'java-docu', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
